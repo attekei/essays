6093988509a5f8407e3ac05790545513c05dda8f
Atte Keinänen <atte.keinanen@gmail.com>
2016-03-10 17:20:32 +0900

    JAHUU it's ready now

--- build-pdf.sh
+++ build-pdf.sh
@@ -7,3 +7,6 @@ pdflatex -interaction=nonstopmode $document_name > /dev/null
 bibtex $document_name
 pdflatex -interaction=nonstopmode $document_name > /dev/null
 pdflatex -interaction=nonstopmode -halt-on-error $document_name
+
+# Word count
+./texcount.pl essay1.tex
