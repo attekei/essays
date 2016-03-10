aafd09d027a2709b7614466ca949ec46b39f75fa
Atte Keinänen <atte@omenapiirakka.local>
2016-03-10 11:18:49 +0900

    Initial commit

---
+++ build-pdf.sh
@@ -0,0 +1,9 @@
+set -uo pipefail
+document_name=$1
+
+# Show only output from bibtex and last last pdflatex run
+# I hate to get excessing amount of output when I run my beautiful scripts ;)
+pdflatex -interaction=nonstopmode $document_name
+bibtex $document_name
+pdflatex -interaction=nonstopmode $document_name
+pdflatex -interaction=nonstopmode -halt-on-error $document_name
